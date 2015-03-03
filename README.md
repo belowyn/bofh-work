0ec9e2e51ebe7ffa77ed5aea6a76a181066b78d8
Øystein Røysland Sørlie <o.r.sorlie@hf.uio.no>
2015-03-03 08:58:48 +0100

    La til en forklaring i brukerrydding, og ryddet vekk noe hjelpeoutput fra testingen

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bofh-work
 Diverse scripts som bruker pybofh for rapportering og verktøy ved HF-IT
+
+Følger også med pybofh fra Stash hos UiO. Fungerende utgave som brukes av scriptene.
--- brukerrydding.py
+++ brukerrydding.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# Script for å hente ut navn og affiliations basert på en liste med brukernavn.
+# Output er en cvs-fil som kan åpnes i Excel. Husk å spesifisere UTF-8 på input i Excel.
 
 import bofh, pprint, getpass, sys, getopt, codecs
 
@@ -20,17 +22,17 @@ def main(argv):
         elif opt in ('-o', '--outputfile'):
             outputfile = arg
 
+    if (outputfile == ''):
+        print 'Gi en fil å skrive til, med opsjon -o'
+        sys.exit()
+
     conn = bofh.connect('https://cerebrum-uio.uio.no:8000', 'data/ca.pem') #kobler til cerebrum
     sid = conn._run_raw_command('login', getpass.getuser(), getpass.getpass()) #ber om passord
     conn._session = sid #session id
 
     brukerrapport = codecs.open(outputfile, encoding='utf-8', mode='w')
-
-
-    #brukerrapport.write('brukernavn,fullt navn,affiliation(s)\n')
     for user in read_userlist(inputfile):
         brukerrapport.write(get_aff(conn, user) + '\n')
-
     brukerrapport.close()    
 
 def read_userlist(file):
@@ -38,26 +40,20 @@ def read_userlist(file):
         userlist = f.read().splitlines()
     return userlist
 
-
 def get_aff(conn, user):
 
     dict = conn.run_command('person_info', user)
     info = user
 
-    print user
+    #print user #dersom man ønsker å liste brukernavnene etterhvert som de blir prossesert
     for item in dict:
         if item.has_key('name'):
             info += ',' + item['name'].replace(' [from Cached]', '')
         if item.has_key('affiliation_1'):
             info += ',' + item['affiliation_1']
-            #print item['affiliation_1']
         if item.has_key('affiliation'):
-            #print item['affiliation']
             info += ',' + item['affiliation']
-
-    #print info
     return info
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
